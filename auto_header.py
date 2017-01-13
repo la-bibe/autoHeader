ff82a8c4746dc1aa7ecff9bdc0328e2466c8712d
Fantin Bibas <fantin.bibas@epitech.eu>
2017-01-13 22:56:12 +0100

    Now find the needed prototypes for each files

--- auto_header.py
+++ auto_header.py
@@ -18,18 +18,24 @@ if len(sys.argv) < 2:
 
 # Regexes
 regFunFull = "^[A-Za-z0-9_]+[ \t\*]+[A-Za-z0-9_]+\(([A-Za-z0-9_]*[ \*]*[A-Za-z0-9_]*,[ \*\n\t]*)*([A-Za-z0-9_]*[ \*]*[A-Za-z0-9_]*)?\)$"
+regFunCalled = "[A-Za-z0-9_]+\("
 cregArgNameComma = re.compile("[ ]*[A-Za-z0-9_]+,")
 cregArgNamePar = re.compile("[ ]*[A-Za-z0-9_]+\)")
 cregSpaces = re.compile("[ \n\t]+")
 cregSpace = re.compile(" ")
 
 functions = []
+functionNames = []
+usedFuncsPerFile = {}
 
 def addFunction(function):
     function = cregArgNameComma.sub(",", function)
     function = cregArgNamePar.sub(")", function)
     function = cregSpaces.sub(" ", function)
     functions.append(function)
+    name = function.split(" ")[1].split("(")[0].replace("*", "")
+    functionNames.append(name)
+    return (name)
 
 def alignFunctions():
     maxLen = 0
@@ -42,19 +48,45 @@ def alignFunctions():
         length = len(func.split(" ")[0])
         functions[i] = cregSpace.sub(" " * (maxLen - length), func, 1)
 
-fileNames = iter(sys.argv)
-next(fileNames)
-for fileName in fileNames:
+def analizeFile(fileName):
+    print ("Analysing \"" + fileName + "\"")
     try:
         file = open(fileName, 'r')
         data = file.read()
-        print ("Analysing \"" + fileName + "\"")
-        for test in re.finditer(regFunFull, data, re.M):
-            addFunction(test.group())
     except:
         error_file(fileName)
+        return
+    localDefinedFuncs = []
+    for function in re.finditer(regFunFull, data, re.M):
+        localDefinedFuncs.append(addFunction(function.group()))
+    localUsedFuncs = []
+    for function in re.finditer(regFunCalled, data, re.M):
+        localUsedFuncs.append(function.group().replace("(", ""))
+    localUsedFuncs = list(set(localUsedFuncs))
+    for function in localDefinedFuncs:
+        localUsedFuncs.remove(function)
+    usedFuncsPerFile[fileName] = localUsedFuncs
+
+
+fileNames = iter(sys.argv)
+next(fileNames)
+for fileName in fileNames:
+    if (fileName.endswith(".c")):
+        analizeFile(fileName)
 
 print ("Aligning the functions")
 alignFunctions()
-print ("\nDone :")
+print ("Done")
+print ("\nPrototypes:\n")
 print ('\n'.join(functions))
+print ("\nNames:\n")
+print ('\n'.join(functionNames))
+print ("\nDefinition of needs for each file:")
+for file, funcs in usedFuncsPerFile.items():
+    print ("\n");
+    print (file + ":");
+    for func in funcs:
+        if func in functionNames:
+            print ("Defined:\t" + functions[functionNames.index(func)])
+        else:
+            print ("Not defined:\t" + func)
