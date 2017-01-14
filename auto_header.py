066dab1feaf2b8be5f505b4492e1f16810cb3a14
Fantin Bibas <fantin.bibas@epitech.eu>
2017-01-14 17:33:54 +0100

    Handle Macros and Makefile && add config file

--- auto_header.py
+++ auto_header.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
+# #########################
+# #      Auto Header      #
+# #########################
+# |Created by Fantin Bibas|
+# |In 2017 Jan.           |
+# |                       |
+# | fantin@bib.as         |
+# | www.bib.as            |
+# +-----------------------+
+
 import sys
 import os
 import re
 
-output = "include/"
-includeFile = "includes.config"
+configFile = "general.conf"
 
 class colors:
     GREEN = "\033[92m"
@@ -26,24 +35,105 @@ def error_file(name):
 if len(sys.argv) < 2:
     error_args()
 
-# Try to open the file
-
 # Regexes
 regFunFull = "^[A-Za-z0-9_]+[ \t\*]+[A-Za-z0-9_]+\(([A-Za-z0-9_]*[ \*]*[A-Za-z0-9_\[\]]*,[ \*\n\t]*)*([A-Za-z0-9_]*[ \*]*[A-Za-z0-9_\[\]]*)?\)$"
 regFunCalled = "[A-Za-z0-9_]+\("
+regMacro = "[^A-Za-z0-9_][A-Z0-9_]*[A-Z]+[A-Z0-9_]*[^A-Za-z0-9_]"
 cregArgNameComma = re.compile("[ ]*[A-Za-z0-9_]+,")
 cregArgNamePar = re.compile("[ ]*[A-Za-z0-9_]+\)")
 cregSpaces = re.compile("[ \n\t]+")
 cregSpace = re.compile(" ")
 
+# Config
+output = "include/"
+includeFile = "functions.conf"
+macroFile = "macros.conf"
+binaryName = "a.out"
+bLetArgNames = 0
+bIncludeHeader = 1
+bDoMakefile = 1
+lineHeader = 10
+
+# Arrays
 functions = []
 functionNames = []
-usedFuncsPerFile = {}
+usedFuncsMacsPerFile = {}
 includes = {}
+macros = {}
 
-def addFunction(function):
-    function = cregArgNameComma.sub(",", function)
-    function = cregArgNamePar.sub(")", function)
+def createMakefile():
+    try:
+        makefile = open("Makefile", "w")
+        makefile.write("CC\t=\tgcc\n\n")
+        makefile.write("RM\t=\trm -f\n\n")
+        makefile.write("CPPFLAGS\t+=\t-I " + output + "\n\n")
+        makefile.write("NAME\t=\t" + binaryName + "\n\n")
+        makefile.write("SRCS\t=\t")
+        for fileName in sorted(usedFuncsMacsPerFile.keys())[:-1]:
+            makefile.write(fileName + "\t\\\n\t\t")
+        makefile.write(sorted(usedFuncsMacsPerFile.keys())[-1] + "\n\n")
+        makefile.write("OBJS\t=\t$(SRCS:.c=.o)\n\n")
+        makefile.write("all:\t$(NAME)\n\n$(NAME):\t$(OBJS)\n\t$(CC) $(OBJS) -o $(NAME) $(FLAGS)\n\n")
+        makefile.write("clean:\n\t$(RM) $(OBJS)\n\n")
+        makefile.write("fclean:\tclean\n\t$(RM) $(NAME)\n\n")
+        makefile.write("re:\tfclean all\n\n")
+        makefile.write(".PHONY:\tall clean fclean re\n\n")
+        makefile.close()
+    except:
+        print (colors.YELLOW + "Warning couldn't create the Makefile" + colors.DEFAULT)
+
+def readConfig(): # Analyse the config file
+    global output
+    global includeFile
+    global macroFile
+    global bIncludeHeader
+    global lineHeader
+    global bDoMakefile
+    global binaryName
+    global bLetArgNames
+    try:
+        config = open(configFile, "r")
+        data = config.read()
+        config.close()
+        data = cregSpaces.sub("", data)
+        sepInc = data.split(";")
+        for inc in sepInc:
+            inc = inc.split(":")
+            if inc[0] == "output":
+                output = inc[1]
+            elif inc[0] == "funcDictionnary":
+                includeFile = inc[1]
+            elif inc[0] == "macrDictionnary":
+                macroFile = inc[1]
+            elif inc[0] == "include":
+                bIncludeHeader = int(inc[1])
+            elif inc[0] == "includeLine":
+                lineHeader = int(inc[1])
+            elif inc[0] == "makefile":
+                bDoMakefile = int(inc[1])
+            elif inc[0] == "binary":
+                binaryName = int(inc[1])
+            elif inc[0] == "argNames":
+                bLetArgNames = int(inc[1])
+    except:
+        try:
+            config = open(configFile, "w")
+            config.write("output:" + output + ";\n")
+            config.write("funcDictionnary:" + includeFile + ";\n")
+            config.write("macrDictionnary:" + macroFile + ";\n")
+            config.write("include:" + str(bIncludeHeader) + ";\n")
+            config.write("includeLine:" + str(lineHeader) + ";\n")
+            config.write("makefile:" + str(bDoMakefile) + ";\n")
+            config.write("binary:" + str(binaryName) + ";\n")
+            config.write("argNames:" + str(bLetArgNames) + ";\n")
+            config.close()
+        except:
+            print (colors.YELLOW + "Warning couldn't create the config file." + colors.DEFAULT)
+
+def addFunction(function): # Add the function to the dictionnary array of known functions
+    if bLetArgNames == 0:
+        function = cregArgNameComma.sub(",", function)
+        function = cregArgNamePar.sub(")", function)
     function = cregSpaces.sub(" ", function)
     functions.append(function)
     name = function.split(" ")[1].split("(")[0].replace("*", "")
@@ -79,11 +169,30 @@ def analizeFile(fileName):
     localUsedFuncs = list(set(localUsedFuncs))
     for function in localDefinedFuncs:
         localUsedFuncs.remove(function)
-    usedFuncsPerFile[fileName] = localUsedFuncs
+    for macro in re.finditer(regMacro, data, re.M): # Macro handling (yes I know i need to clean this)
+        macro = macro.group()[1:-1]
+        localUsedFuncs.append(macro)
+    localUsedFuncs = list(set(localUsedFuncs))
+    usedFuncsMacsPerFile[fileName] = localUsedFuncs
+    if bIncludeHeader == 1:
+        try:
+            file = open(fileName, "r")
+            data = file.readlines()
+            file.close()
+            data.insert(lineHeader, "#include \"" + fileName.split("/")[-1].replace(".c", ".h\"\n\n"))
+            file = open(fileName, "w")
+            data = "".join(data)
+            file.write(data)
+            file.close()
+        except:
+            error_file(fileName)
 
 def addDblIncSec(file, name):
     name = name.split("/")[-1].upper().replace(".", "_") + "_"
     file.write("#ifndef " + name + "\n" + "#  define " + name + "\n\n")
+
+
+readConfig()
 
 fileNames = iter(sys.argv)
 next(fileNames)
@@ -91,14 +200,12 @@ for fileName in fileNames:
     if (fileName.endswith(".c")):
         analizeFile(fileName)
 
-
-
 print ("Aligning the functions")
 alignFunctions()
 print ("Creating the output folder")
 if not os.path.exists(output):
     os.makedirs(output)
-print ("Opening the " + includeFile + " file")
+print ("Opening \"" + includeFile + "\"")
 try:
     config = open(includeFile, "r")
     data = config.read()
@@ -109,9 +216,21 @@ try:
         inc = inc.split(":")
         includes[inc[0]] = inc[1]
 except:
-    print (colors.YELLOW + "Warning: no config file found, you should add one" + colors.DEFAULT)
+    print (colors.YELLOW + "Warning: \"" + includeFile + "\" not found." + colors.DEFAULT)
+print ("Opening \"" + macroFile + "\"")
+try:
+    config = open(macroFile, "r")
+    data = config.read()
+    config.close()
+    data = cregSpaces.sub("", data)
+    sepInc = data.split("-")
+    for inc in sepInc:
+        inc = inc.split(":")
+        macros[inc[0]] = inc[1]
+except:
+    print (colors.YELLOW + "Warning: \"" + macroFile + "\" not found." + colors.DEFAULT)
 print ("Creating the files")
-for fileName, funcs in usedFuncsPerFile.items():
+for fileName, funcs in usedFuncsMacsPerFile.items():
     if funcs:
         tempName = output + fileName.split("/")[-1].replace(".c", ".h")
         print ("Creating \"" + tempName + "\"")
@@ -126,17 +245,27 @@ for fileName, funcs in usedFuncsPerFile.items():
                         found = 1
                         if key != "void":
                             neededIncludes.append(key)
+                for key, value in macros.items():
+                    if func in value.split(";"):
+                        found = 1
+                        if key != "void":
+                            neededIncludes.append(key)
                 if found == 0:
-                    print (colors.YELLOW + "Warning: function \"" + func + "\" not found in the config file" + colors.DEFAULT)
+                    print (colors.YELLOW + "Warning: function or macro \"" + func + "\" not found in the config file" + colors.DEFAULT)
         neededIncludes = list(set(neededIncludes))
         for inc in neededIncludes:
-            file.write("#  include <" + inc + ">\n")
+            if inc[0] == '!':
+                file.write("#  include \"" + inc[1:] + "\"\n")
+            else:
+                file.write("#  include <" + inc + ">\n")
         if neededIncludes:
             file.write("\n")
         for func in funcs:
             if func in functionNames:
                 file.write(functions[functionNames.index(func)] + ";\n")
-
         file.write("\n#endif")
         file.close()
+if bDoMakefile == 1:
+    print ("Creating the Makefile")
+    createMakefile()
 print ("Done")
