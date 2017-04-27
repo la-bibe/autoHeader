a55b89df28275466c6b3a7c5de5dadf870c367b2
yannick.suc <yannick.suc@epitech.eu>
2017-04-27 14:52:15 +0200

    Syntaxic hightlighting update

--- autoheader-mode.el
+++ autoheader-mode.el
@@ -1,7 +1,9 @@
 (setq autoheader-highlights
-      '(("[_a-zA-Z0-9]*:.*;" . font-lock-function-name-face)
-	("!?[_a-zA-Z0-9./]*:" . font-lock-type-face)
-        ("-?=?" . font-lock-keyword-face)))
+      '(("[a-zA-Z]*:.*;" . font-lock-builtin-face)
+	("!?[_a-zA-Z0-9\.\/]*:" . font-lock-type-face)
+	("[ \t]*[\-_a-zA-Z0-9]*;" . font-lock-function-name-face)
+        ("-?=?" . font-lock-keyword-face)
+	))
 
 (define-derived-mode autoheader-mode fundamental-mode "autoheader"
   "Major mode for editing .conf files."
