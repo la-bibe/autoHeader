883adae0b65d05446d5ccbf895740f89c03c30fa
yannick.suc <yannick.suc@epitech.eu>
2017-04-29 14:11:52 +0200

    Adding keyboard macros to move cursor betweens the differents parts of .conf files

--- autoheader-mode.el
+++ autoheader-mode.el
@@ -5,6 +5,16 @@
         ("-?=?" . font-lock-keyword-face)
 	))
 
+(global-set-key (kbd "C-<down>") 'autoheader-move-next)
+(global-set-key (kbd "C-<up>") 'autoheader-move-prev)
+
+(defun autoheader-move-next ()
+  (interactive)
+  (re-search-forward "!?[_a-zA-Z0-9\.\/]*:$"))
+(defun autoheader-move-prev ()
+  (interactive)
+  (search-backward-regexp "!?[_a-zA-Z0-9\.\/]*:$"))
+
 (define-derived-mode autoheader-mode fundamental-mode "autoheader"
   "Major mode for editing .conf files."
   (setq font-lock-defaults '(autoheader-highlights)))
